265f6321ea467364ca4bae7bb180a645e62d822e
Dal-ya <hyansky@daum.net>
2020-04-20 23:54:05 +0900

    Add : 404, 500 미들웨어 추가 --------------
    app.js 에 404, 500 미들웨어 추가

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 
 const app = express();
+app.set('port', process.env.NODE_ENV || 5001);
 
-app.use('/', (req, res) => {
+app.use('/hello', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(5001, () => {
-  console.log('5001 server start...');
+app.use((req, res, next) => {
+  const err = new Error('404 NOT FOUND');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  let sendMessage;
+  if (err.status === 404) {
+    sendMessage = '<h1>404 찾으시는 페이지가 없습니다</h1>';
+  } else {
+    sendMessage = '<h1>서버 에러</h1>';
+  }
+  const message =
+    req.app.get('env') === 'development' ? err.message : sendMessage;
+  res.send(message);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')} server start...`);
 });
